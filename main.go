ca5ea9c8c84ab1a81087fa9f2818ebb1cc85e942
ashmit2c1 <codeatashmit@gmail.com>
2025-02-01 09:25:15 +0530

    Initial set up for project

--- main.go
+++ main.go
@@ -3,6 +3,8 @@ package main
 import "github.com/gin-gonic/gin"
 
 func main() {
+	// CREATING SERVER INSTANCE
 	server := gin.Default()
+	// RUNNING THE SERVER ON PORT 8080 ON LOCAL HOST
 	server.Run(":8080")
 }
